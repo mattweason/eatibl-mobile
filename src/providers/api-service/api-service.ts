f603fb74fd6cd70c517ef11812964e9de2cf3486
Shayan Nahrvar <shayan@oasiscode.com>
2018-02-19 12:50:52 -0500

    Updated API service so it uses the latest angular/common/http

--- src/providers/api-service/api-service.ts
+++ src/providers/api-service/api-service.ts
@@ -1,4 +1,4 @@
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
@@ -16,12 +16,12 @@ export class ApiServiceProvider {
 
   private url: string = "http://localhost:3000/api/";
 
-  constructor(private http: Http) {}
+  constructor(private http: HttpClient) {}
 
   makeCall(url){
-    return this.http.get(this.url+ url)
-      .map(this.extractData)
-      .do(this.logResponse)
+    return this.http.get(this.url+ url);
+      //.map(this.extractData)
+      //.do(this.logResponse)
       // .catch(this.catchError)
   }
 
