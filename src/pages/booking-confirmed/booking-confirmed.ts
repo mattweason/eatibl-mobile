08faf2fef5467d20c37c607f9fbd63351ad93e49
Matt <matt@oasiscode.com>
2018-03-26 15:12:23 -0400

    Added cancel booking process.

--- src/pages/booking-confirmed/booking-confirmed.ts
+++ src/pages/booking-confirmed/booking-confirmed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { ApiServiceProvider } from "../../providers/api-service/api-service";
 
 import { FunctionsProvider } from '../../providers/functions/functions';
 
@@ -18,8 +19,9 @@ export class BookingConfirmedPage {
   restaurant: any;
   booking: any;
   dateObject = {} as any;
+  response: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private functions: FunctionsProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private functions: FunctionsProvider, private API: ApiServiceProvider, public alertCtrl: AlertController) {
     this.restaurant = navParams.get('restaurant');
     this.booking = navParams.get('booking');
     this.buildDateObject();
@@ -39,6 +41,58 @@ export class BookingConfirmedPage {
     this.dateObject.month = month;
     this.dateObject.date = date;
     this.dateObject.day = day;
+  }
+
+  cancelBooking(){
+    let alert = this.alertCtrl.create({
+      title: 'Cancel Booking',
+      message: 'Are you sure you want to cancel this booking?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.API.makeCall('booking/'+this.booking._id+'/disable').subscribe(response => {
+              this.response = response;
+              if(this.response.message == 'error')
+                this.errorAlert();
+              if(this.response.disabled == 1)
+                this.successAlert();
+            });
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  errorAlert(){
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'Sorry, there was an error cancelling your booking. Please try again.',
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  successAlert(){
+    let alert = this.alertCtrl.create({
+      title: 'Booking Cancelled',
+      subTitle: 'Your booking at '+this.restaurant.name+' was successfully cancelled.',
+      buttons: [{
+        text: 'Dismiss',
+        handler: () => {
+          this.navCtrl.pop();
+        }
+      }]
+    });
+    alert.present();
   }
 
 }
