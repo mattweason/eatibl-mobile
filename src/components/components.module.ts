e6939e4c28edb7b0599fd9209315d9ae6c2f6874
Shayan Nahrvar <shayannahrvar@Shayans-MacBook-Air.local>
2018-05-10 11:44:11 -0400

    Updates for package json and config to include ios stuff, and a few updated packages.

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BookingCardComponent } from './booking-card/booking-card';
-@NgModule({
-	declarations: [BookingCardComponent],
-	imports: [],
-	exports: [BookingCardComponent]
-})
-export class ComponentsModule {}
