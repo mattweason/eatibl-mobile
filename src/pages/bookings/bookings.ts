1a50d38feb6764edb5d39cbfee5c5d8d0dc329a3
Matt <matt@oasiscode.com>
2018-05-28 14:53:49 -0400

    Updates to booking-card and refined sorting for upcoming and historical bookings.

--- src/pages/bookings/bookings.ts
+++ src/pages/bookings/bookings.ts
@@ -44,24 +44,39 @@ export class BookingsPage {
       if(val){
         this.user = decode(val);
         this.API.makePost('booking/user', {email: this.user.email}).subscribe(data => {
-          this.sortBookings(data);
+          this.filterAndSortBookings(data);
         });
       }
     });
   }
 
-  sortBookings(data){
+  filterAndSortBookings(data){
     var functions = this.functions;
+    //Filter and sort upcoming bookings
     this.bookingUpcoming = _.filter(data, function(booking){
       var date = moment(booking.date).format('L');
       var time = functions.formatClockTime(booking.time, true);
       return moment(date+' '+time).isAfter(moment());
     });
+    this.bookingUpcoming = _.sortBy(this.bookingUpcoming, function(booking){ //Sort by secondary first
+      return booking.time;
+    });
+    this.bookingUpcoming = _.sortBy(this.bookingUpcoming, function(booking){ //Sort by primary second
+      return booking.date;
+    });
+    //Filter and sort booking history
     this.bookingHistory = _.filter(data, function(booking){
       var date = moment(booking.date).format('L');
       var time = functions.formatClockTime(booking.time, true);
       return moment(date+' '+time).isBefore(moment());
     });
+    this.bookingHistory = _.sortBy(this.bookingHistory, function(booking){ //Sort by secondary first
+      return booking.time;
+    });
+    this.bookingHistory = _.sortBy(this.bookingHistory, function(booking){ //Sort by primary second
+      return booking.date;
+    });
+    this.bookingHistory = this.bookingHistory.reverse(); //Reverse the order so most recent is first
   }
 
   signUp(){
