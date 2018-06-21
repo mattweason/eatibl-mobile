3a72ab206d6ade55eb8e67667cd8852d56d1ddf6
Shayan Nahrvar <shayan@oasiscode.com>
2018-06-21 12:38:39 -0400

    Environment files are now being tracked for ionic pro build process.

---
+++ src/environment/environment.dev.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Owner on 2018-02-23.
+ */
+export const ENV = {
+  mode: 'Development',
+  API: 'https://test.eatibl.com/api/'
+}
---
+++ src/environment/environment.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Owner on 2018-02-23.
+ */
+export const ENV = {
+  mode: 'Production',
+  API: 'https://eatibl.com/api/'
+}
